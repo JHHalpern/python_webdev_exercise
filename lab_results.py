026748602fe7278b337ab508bcc647ac36dd2512
Josh <jhhalpern@gmail.com>
2024-10-31 21:49:09 -0400

    update filtering criteria

--- lab_results.py
+++ lab_results.py
@@ -5,9 +5,8 @@ def get_results(name):
     all_results = pd.read_csv("data/patient_tb.csv")
     name_matches = all_results[all_results["PatientFirstName"] == name]
 
-    # filtered to remove duplicate rows where one patient takes 
-    # the same test multiple times on one day and recieves the same result
-    filtered_results = name_matches.drop_duplicates(subset=["PatientID", "MostRecentTestDate", "TestName", "MostRecentLabResult"])
+    # filtered to remove duplicate rows where one patient takes the same test multiple times on one day
+    filtered_results = name_matches.drop_duplicates(subset=["PatientID", "MostRecentTestDate", "TestName"])
 
     # Convert MostRecentTestDate string to datetime, then sort, then convert back to string
     filtered_results["MostRecentTestDate"] = pd.to_datetime(filtered_results["MostRecentTestDate"])
